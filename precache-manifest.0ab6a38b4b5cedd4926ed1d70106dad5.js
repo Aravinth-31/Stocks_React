47e03751d6dc4d9be9ba693ad6d794bb1f7d5bf5
Aravinth-thunder <varavinth321@gmail.com>
2020-07-02 14:24:08 +0530

    Updates

---
+++ precache-manifest.0ab6a38b4b5cedd4926ed1d70106dad5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849da41aa290f9ecc65c73ea31737ba3",
+    "url": "/Stocks/index.html"
+  },
+  {
+    "revision": "bb102fb33d992d0f9d22",
+    "url": "/Stocks/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "7e841cbc57e813ea1cc6",
+    "url": "/Stocks/static/css/main.9155f3c7.chunk.css"
+  },
+  {
+    "revision": "bb102fb33d992d0f9d22",
+    "url": "/Stocks/static/js/2.a5781cea.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Stocks/static/js/2.a5781cea.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e841cbc57e813ea1cc6",
+    "url": "/Stocks/static/js/main.7a635d37.chunk.js"
+  },
+  {
+    "revision": "5925806ede3247353455",
+    "url": "/Stocks/static/js/runtime-main.c0336dfd.js"
+  }
+]);
